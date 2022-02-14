ce19003d372ee7f7ba6ebc8b39f65bc261a56fa1
afirsov <a.firsov@dzento.com>
2022-02-14 13:07:15 +0200

    Update

--- src/file/resource/parser/ResourceSpineParser.js
+++ src/file/resource/parser/ResourceSpineParser.js
@@ -1,3 +1,6 @@
+import { AtlasAttachmentLoader, SkeletonJson, Spine } from "@pixi-spine/runtime-3.8";
+import { TextureAtlas } from "pixi-spine";
+import { Application, Graphics, Texture } from "pixi.js";
 import FileType from "../../FileType";
 import ResourceStruct from "../ResourceStruct";
 import ResourceType from "../ResourceType";
@@ -8,12 +11,29 @@ import AbstractResourceParser from "./AbstractResourceParser";
  */
 export default class ResourceSpineParser extends AbstractResourceParser {
 
+    static get texture() {
+        if (!ResourceSpineParser._texture) {
+            const graphics = new Graphics();
+            graphics.beginFill(0xff0000, 0.2);
+            graphics.drawRect(0, 0, 100, 100);
+            ResourceSpineParser._texture = Application.application.renderer.generateTexture(graphics, {});
+        }
+        return ResourceSpineParser._texture;
+    }
+
+    get resourceRequireNameList() {
+        this.resourceStruct;
+        debugger;
+        return [];
+    }
+
     guard( contentStruct ) {
         return contentStruct.type === FileType.JSON && this.checkJSONContentStruct( contentStruct )
     }
     
     parse( contentStruct ) {
         this.resourceCreate( contentStruct );
+        // this.spineJSONParse();
     }
 
     resourceCreate( contentStruct ) {
@@ -30,13 +50,89 @@ export default class ResourceSpineParser extends AbstractResourceParser {
     }
 
     checkJSONContentStruct( contentStruct ) {
-        const jsonData = JSON.parse( contentStruct.result );
-
+        const jsonData = this.jsonParseToObjectGet( contentStruct.result );
         if ( !jsonData ) return false;
         if ( !jsonData.skeleton || !jsonData.skeleton.spine ) return false;
         if ( !jsonData.bones || !jsonData.bones.length ) return false;
         if ( !jsonData.slots || !jsonData.slots.length ) return false;
+        if ( !jsonData.skins || !jsonData.skins.length ) return false;
+        
         return true;
     };
 
+    jsonParseToObjectGet( jsonText ) {
+        try {
+            return JSON.parse( jsonText );
+        } catch (error) {
+            console.log( error.toString() );
+        }
+        
+        return {};
+    }
+
+    jsonGet() {
+        return false;
+    }
+
+    spineJSONParse() {
+        this.json = this.jsonParseToObjectGet( this.resourceStruct.contentStruct.result );
+        
+        this.attachmentNameList = this.skinsAttachmentNamesGet( this.json.skins );
+        debugger;
+
+        const spineAtlas = new TextureAtlas( this.json,
+            function( name, callback ) {
+                callback(Texture.from(name));
+            }
+        );
+
+        const spineAtlasLoader = new AtlasAttachmentLoader( spineAtlas );
+        const spineJsonParser = new SkeletonJson( spineAtlasLoader );
+        const spineData = spineJsonParser.readSkeletonData( this.json );
+
+        const spine = new Spine( spineData );
+        debugger;
+    }
+
+
+    //
+    // SKINS
+    //
+
+    skinsAttachmentNamesGet( skins ) {
+        if ( !skins || !skins.length ) return;
+
+        for ( let i = 0; i < skins.length; i++ ) {
+            const skin = skins[ i ];
+            // const attachment = this.skinsAttachmentParse( skin.attachments );
+
+            for (const name in skin.attachments) {
+                const attachment = skin.attachments[name];
+                for (const attachTextureName in attachment) {
+                    const attachTextureData = attachment[attachTextureName];
+                    Texture.addToCache(ResourceSpineParser.texture, attachTextureName);
+                }
+            }
+        }
+    }
+
+    skinAttachmentsParse( attachmentObject ) {
+        const { name, attachments } = attachmentObject;
+        return {
+            ...AttachmentStruct,
+            name,
+            attachments: this
+        }
+    }
+
+}
+
+const AttachmentsStruct = {
+    name: undefined,
+    attachments: []
+}
+
+const AttachmentStruct = {
+    name: undefined,
+    attachments: []
 }
